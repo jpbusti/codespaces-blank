6b98e596f0b376793224ba51ee7e9cf8d50eb3f0
Paulo Poveda <126352326+paulopoveda@users.noreply.github.com>
2025-07-31 22:14:43 +0000

    hghgh

--- SRC/add_plus_array.java
+++ SRC/add_plus_array.java
@@ -1,13 +1,13 @@
 public class add_plus_array{
     public static void main(String[] arg) {
-        int vector[] = new int[100];
+        int v[] = new int[100];
         int sum = 0;
         int n=80;
-        vector[1]=20;
-        vector[2]=20;
-        vector[3]=1000;
+        v[1]=20;
+        v[2]=20;
+        v[3]=1000;
         for( int i=1; i<=n; i++){
-            sum = sum + vector[i];
+            sum = sum + v[i];
         }
         System.out.println("El resultado de la suma es" + sum);
     }
